d892712e251bdb49a7230c109166c8ae3fbe796f
Arne Lauryssens <arnelauryssens@hotmail.com>
2016-02-11 23:32:56 +0100

    changes for working with webstorm

--- src/main/java/be/kdg/kandoe/frontend/controllers/HelloWorldController.java
+++ src/main/java/be/kdg/kandoe/frontend/controllers/HelloWorldController.java
@@ -3,6 +3,7 @@ package be.kdg.kandoe.frontend.controllers;
 import be.kdg.kandoe.frontend.DTO.*;
 import be.kdg.kandoe.backend.dom.other.Organisation;
 import be.kdg.kandoe.backend.services.api.OrganisationService;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class HelloWorldController {
 
+    private final Logger logger = Logger.getLogger(HelloWorldController.class);
     private final OrganisationService organisationService;
 
     @Autowired
@@ -20,11 +22,24 @@ public class HelloWorldController {
         this.organisationService = organisationService;
     }
 
-    @RequestMapping(value = "/", method = RequestMethod.GET)
-    public ModelAndView index() {
+    //todo delete if it turns out we don't need it
+    /*@RequestMapping(value = "/", method = RequestMethod.GET)
+     public ModelAndView index() {
+
+        logger.info("testetestetqeteqteer");
         ModelAndView mav = new ModelAndView();
-        mav.setViewName("home");
+        mav.setViewName("index");
 
         return mav;
     }
+
+    @RequestMapping(value = "/index", method = RequestMethod.GET)
+    public ModelAndView home() {
+
+        logger.info("testetestetqeteqteer");
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName("index");
+
+        return mav;
+    }       */
 }
