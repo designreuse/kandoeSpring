7725b714b9ef9c02bfe6b64f2e46a63e927ae689
memelaos <jordan.parezys@student.kdg.be>
2016-02-16 14:20:56 +0100

    login/register basics frontend

--- src/main/webapp/app/components/home.js
+++ src/main/webapp/app/components/home.js
@@ -1,11 +1,9 @@
 System.register(["angular2/core"], function(exports_1) {
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
-        if (typeof Reflect === "object" && typeof Reflect.decorate === "function") return Reflect.decorate(decorators, target, key, desc);
-        switch (arguments.length) {
-            case 2: return decorators.reduceRight(function(o, d) { return (d && d(o)) || o; }, target);
-            case 3: return decorators.reduceRight(function(o, d) { return (d && d(target, key)), void 0; }, void 0);
-            case 4: return decorators.reduceRight(function(o, d) { return (d && d(target, key, o)) || o; }, desc);
-        }
+        var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+        if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+        else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+        return c > 3 && r && Object.defineProperty(target, key, r), r;
     };
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
@@ -20,17 +18,29 @@ System.register(["angular2/core"], function(exports_1) {
         execute: function() {
             Home = (function () {
                 function Home() {
+                    this.loginFormVisible = false;
+                    this.registerFormVisible = false;
                 }
                 Home.prototype.login = function () {
-                    alert("nu ga je naar login pagina");
+                    if (this.loginFormVisible) {
+                        this.loginFormVisible = false;
+                    }
+                    else {
+                        this.loginFormVisible = true;
+                    }
                 };
                 Home.prototype.register = function () {
-                    alert("nu ga je registreren");
+                    if (this.registerFormVisible) {
+                        this.registerFormVisible = false;
+                    }
+                    else {
+                        this.registerFormVisible = true;
+                    }
                 };
                 Home = __decorate([
                     core_1.Component({
                         selector: 'home',
-                        template: "\n\n<div class=\"home\">\n    <h1 class=\"jumbotron\">Welcome to Kandoe</h1>\n    <p class=\"jumbotron\">Free platform for making decisions <br>\n        Easy to use<br>\n        You can participate with a lot of people</p>\n    <button (click)=\"login()\">Log in</button>\n    <button (click)=\"register()\">Register</button>\n\n     "
+                        template: "\n\n<div class=\"home\">\n    <h1 class=\"jumbotron\">Welcome to Kandoe</h1>\n    <div class=\"background-img\">\n    <h2 class=\"col-md-12 intro\">Free platform for making decisions <br>\n        Easy to use<br>\n        You can participate with a lot of people</h2>\n\n        <div *ngIf=\"loginFormVisible == false\">\n            <button class=\"col-md-3 col-md-offset-2\" (click)=\"login()\">Log in</button>\n        </div>\n        <div *ngIf=\"loginFormVisible == true\">\n            <form method=\"post\">\n                <input type=\"text\" name=\"username\" />\n                <input type=\"password\" name=\"password\"/>\n                <input type=\"submit\" name=\"login\" value=\"submit\"/>\n            </form>\n        </div>\n        <div *ngIf=\"registerFormVisible == false\">\n            <button class=\"col-md-3 col-md-offset-1\" (click)=\"register()\">Register</button>\n        </div>\n        <div *ngIf=\"registerFormVisible == true\">\n             <form method=\"post\">\n                <input type=\"text\" name=\"username\" />\n                <input type=\"password\" name=\"password\"/>\n                <input type=\"submit\" name=\"login\" value=\"submit\"/>\n            </form>\n        </div>\n    </div>\n</div>\n        "
                     }), 
                     __metadata('design:paramtypes', [])
                 ], Home);
--- src/main/webapp/app/components/home.ts
+++ src/main/webapp/app/components/home.ts
@@ -6,21 +6,59 @@ import {Component} from "angular2/core";
 
 <div class="home">
     <h1 class="jumbotron">Welcome to Kandoe</h1>
-    <p class="jumbotron">Free platform for making decisions <br>
+    <div class="background-img">
+    <h2 class="col-md-12 intro">Free platform for making decisions <br>
         Easy to use<br>
-        You can participate with a lot of people</p>
-    <button (click)="login()">Log in</button>
-    <button (click)="register()">Register</button>
+        You can participate with a lot of people</h2>
 
-     `
+        <div *ngIf="loginFormVisible == false">
+            <button class="col-md-3 col-md-offset-2" (click)="login()">Log in</button>
+        </div>
+        <div *ngIf="loginFormVisible == true">
+            <form method="post">
+                <input type="text" name="username" />
+                <input type="password" name="password"/>
+                <input type="submit" name="login" value="submit"/>
+            </form>
+        </div>
+        <div *ngIf="registerFormVisible == false">
+            <button class="col-md-3 col-md-offset-1" (click)="register()">Register</button>
+        </div>
+        <div *ngIf="registerFormVisible == true">
+             <form method="post">
+                <input type="text" name="username" />
+                <input type="password" name="password"/>
+                <input type="submit" name="login" value="submit"/>
+            </form>
+        </div>
+    </div>
+</div>
+        `
 })
 
 export class Home {
-    login(){
-        alert("nu ga je naar login pagina");
+    loginFormVisible:boolean
+    registerFormVisible:boolean
+
+    constructor(){
+        this.loginFormVisible=false;
+        this.registerFormVisible=false;
     }
-    register(){
-        alert("nu ga je registreren");
+
+    login() {
+        if(this.loginFormVisible){
+            this.loginFormVisible=false;
+        } else {
+            this.loginFormVisible=true;
+        }
+    }
+
+    register() {
+        if(this.registerFormVisible){
+            this.registerFormVisible=false;
+        } else {
+            this.registerFormVisible=true;
+        }
     }
 
 }
