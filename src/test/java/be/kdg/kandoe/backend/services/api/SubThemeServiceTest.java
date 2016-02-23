3c7c7a2e54866ff21313bfbafbfb3dcc38c8aa4e
Annelies <annelies.geerts.1@student.kdg.be>
2016-02-23 11:22:04 +0100

    subtheme

--- src/test/java/be/kdg/kandoe/backend/services/api/SubThemeServiceTest.java
+++ src/test/java/be/kdg/kandoe/backend/services/api/SubThemeServiceTest.java
@@ -17,47 +17,7 @@ import org.springframework.test.context.transaction.TransactionConfiguration;
 import javax.transaction.Transactional;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotNull;
 
-/*@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = BackendContextConfig.class)
-@TransactionConfiguration(defaultRollback = true)
-@Transactional
-public class SubThemeServiceTest {
-
-    @Autowired
-    private SubThemeService subThemeService;
-    @Autowired
-    private ThemeService themeService;
-
-
-   @Test
-    public void testSaveSubTheme() throws Exception {
-        /*Theme theme = new Theme("KdG");
-        theme.setDescription("KdGParent description");
-        themeService.saveTheme(theme, 1);
-
-        SubTheme subtheme = new SubTheme("KdGsub", themeService.findThemeById(1));
-        subtheme.setDescription("kdgSubTheme description");
-        subThemeService.saveSubTheme(subtheme, 1);
-        SubTheme subTheme = subThemeService.findSubThemeByName("KdGsub");
-
-        assertEquals(theme.getThemeName(),"KdG");
-
-        subtheme.setThemeName("KdGsubUpdate");
-        subThemeService.updateSubTheme(subtheme);
-        System.out.println(subTheme);
-
-        System.out.println(subTheme.getId());
-        System.out.println(subTheme.getThemeName());
-       assertNotNull("The new subthema should have an id", subTheme.getId());
-
-        subThemeService.deleteSubThemeById(2);
-        assertEquals("The subtheme should be deleted", themeService.findThemeById(2), null);       */
-
-    }
-}*/
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = BackendContextConfig.class)
