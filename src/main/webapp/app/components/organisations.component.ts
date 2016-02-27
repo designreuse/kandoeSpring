5824a430797b972584608b42cabd7d040b7c4cba
amy <amy_peerlinck@hotmail.com>
2016-02-27 13:27:28 +0100

    Organisations Front-End Search & Filter

--- src/main/webapp/app/components/organisations.component.ts
+++ src/main/webapp/app/components/organisations.component.ts
@@ -9,17 +9,31 @@ import {Service} from "../service/service";
 @Component({
     selector: 'organisations',
     template: `
-    <div class="panel-body container">
+
+    <div class="container">
+        <div class="input-append">
+            <input class="span2 search-query" id="input-search" type="search" placeholder="Search organisations..." >
+            <div class="btn-group">
+                <button type="button" class="btn btn-primary dropdown-toggle" id ="filter" data-toggle="dropdown">
+                <span class="glyphicon glyphicon-filter"></span>
+                <span class="caret"></span>Filter</button>
+                <ul class="dropdown-menu">
+                    <li><a href="#">Name</a></li>
+                    <li><a href="#">Id</a></li>
+                    <li><a href="#">Address</a></li>
+                </ul>
+            </div>
+        </div>
     	<div class="row">
 			<div class="col-xs-12 col-sm-offset-2 col-sm-8">
-				<ul>
+				<ul class="searchable-container">
 				    <div *ngFor="#organisation of organisations" class="event-list col-1-4">
                     <!--<p>OrganisationID : {{organisation.organisationId}}</p>-->
-					<li>
-						<div class="id"><p>{{organisation.organisationId}}</p></div>
+					<li class="items">
+					    <div class="id"><p>{{organisation.organisationId}}</p></div>
 						<img alt="KdG" [src]="organisation.logoUrl" />
 						<div class="info">
-							<h2 class="title">{{organisation.organisationId}} {{organisation.organisationName}}</h2>
+							<h2 class="title">{{organisation.organisationName}}</h2>
 							<p class="desc">{{organisation.address}}</p>
 						</div>
 						<div class="social">
@@ -47,7 +61,15 @@ export class OrganisationsComponent implements OnInit {
 
     ngOnInit() {
         this._service.getAllOrganisations().subscribe((organisations:Organisation[])=> this.organisations = organisations);
-       console.log(this.organisations.length);
-    }
+        console.log(this.organisations.length);
 
+        $('#input-search').on('keyup', function () {
+            var rex = new RegExp($(this).val(), 'i');
+            $('.searchable-container .items').hide();
+            $('.searchable-container .items').filter(function () {
+                return rex.test($(this).text());
+            }).show();
+        });
+
+    }
 }
