91309a96d600d6427091e53baf27fecff67b2c4a
sennew <sennewens@hotmail.com>
2016-03-07 20:22:34 +0100

    Update boot.ts

--- src/main/webapp/app/boot.ts
+++ src/main/webapp/app/boot.ts
@@ -38,8 +38,8 @@ bootstrap(AppComponent,
     provide(ROUTER_PRIMARY_COMPONENT, {useValue: AppComponent}),
     provide(APP_BASE_HREF, {useValue: "/"}),
     provide(LocationStrategy, {useClass: HashLocationStrategy}),
-   provide('App.BackEndPath', {useValue: "http://localhost:9966/Kandoe/api/"}),
-    provide('App.DevPath', {useValue: "http://localhost:9966/Kandoe/api/"})
+   provide('App.BackEndPath', {useValue: "https://wildfly-teamiip2kdgbe.rhcloud.com/api/"}),
+    provide('App.DevPath', {useValue: "https://wildfly-teamiip2kdgbe.rhcloud.com/api/"})
 ]);
 //  https://wildfly-teamiip2kdgbe.rhcloud.com/api/
 //  http://localhost:9966/Kandoe/api/
