e5e52d1ec6a5b2f079781aac958cde7fb1785d3e
michaelkees <michael.kees@student.kdg.be>
2016-03-08 15:27:35 +0100

    Circle SVG

--- src/main/webapp/app/components/sessions/sessionDetail.component.ts
+++ src/main/webapp/app/components/sessions/sessionDetail.component.ts
@@ -15,62 +15,7 @@ import {Person} from "../../DOM/users/person";
 @Component({
     selector: 'session-detail',
     directives: [ROUTER_DIRECTIVES, RouterLink],
-    template: `
-    <nav class="navbar navbar-inverse navbar-fixed-top " role="navigation">
-        <div class="container">
-            <div class="navbar-header ">
-                <button type="button" class="navbar-toggle" data-toggle="dropdown-toggle"
-                        data-target="#bs-example-navbar-collapse-1">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="./">
-                    KAN<span>DOE</span></a>
-            </div>
-            <ul class="nav navbar-nav navwidth">
-                <li><a [routerLink]="['/LoggedInHome']">   KANDOES   </a></li>
-                <li > <a [routerLink]="['/Organisations']">  ORGANISATIONS   </a> </li>
-                <li class="active"> <a href="#">    THEMES   </a> </li>
-            </ul>
-            <ul class="nav navbar-nav navbar-right">
-                <li><img class="img-responsivenav img-thumbnailnav" id="profile-picturenav"  [src]="getImageSrc(user.profilePicture)">
-                </li>
-                <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">{{user.person.firstname}} <span class="caret"></span></a>
-                    <ul class="dropdown-menu">
-                        <li> <a class="glyphicon" [routerLink]="['/Userprofile']">Edit profile</a></li>
-                        <li role="separator" class="divider"></li>
-                        <li> <a class="glyphicon glyphicon-log-out"  (click)="logout()"> Logout</a></li>
-                    </ul>
-                </li>
-            </ul>
-        </div>
-    </nav>
-    <header>
-        <div class="container clearfix">
-            <h3>THEME</h3>
-        </div>
-    </header>
-    <div class="container main">
-        <ol class="breadcrumb">
-            <li><a [routerLink]="['/LoggedInHome']"> Kandoes</a></li>
-            <li><a [routerLink]="['/Themes']">Themes</a></li>
-            <li class="active"> Add theme</li>
-        </ol>
-        <div class="row">
-
-        </div>
-        <div class="row">
-            <div class="center-container">
-                <svg id="circlesvg" width="100%" height="100%">
-                    <circle *ngFor="#siz of size" [attr.cx]="calculateWidthCentre()" [attr.cy]="calculateHeightCentre()" [attr.r]="60 + (60*siz)" stroke="#fff" stroke-width="8" fill="#1E8BC3"/>
-                </svg>
-            </div>
-        </div>
-    </div>
-    `
+    templateUrl: 'app/components/sessions/sessionDetail.html',
 })
 
 export class SessionDetailComponent implements OnInit{
