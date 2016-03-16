6a8d680c7cb6da1bda159e95b6f05d9d4935c97d
Arne Lauryssens <arne.lauryssens@student.kdg.be>
2016-03-16 12:42:59 +0100

    added subThemeDetail.component.js

---
+++ src/main/webapp/app/components/themes/subThemes/subThemeDetail.component.js
@@ -0,0 +1,43 @@
+System.register(['angular2/core', "../../../security/TokenHelper", "angular2/router"], function(exports_1) {
+    var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+        var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+        if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+        else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+        return c > 3 && r && Object.defineProperty(target, key, r), r;
+    };
+    var __metadata = (this && this.__metadata) || function (k, v) {
+        if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+    };
+    var core_1, TokenHelper_1, router_1;
+    var SubThemeDetailComponent;
+    return {
+        setters:[
+            function (core_1_1) {
+                core_1 = core_1_1;
+            },
+            function (TokenHelper_1_1) {
+                TokenHelper_1 = TokenHelper_1_1;
+            },
+            function (router_1_1) {
+                router_1 = router_1_1;
+            }],
+        execute: function() {
+            SubThemeDetailComponent = (function () {
+                function SubThemeDetailComponent() {
+                }
+                SubThemeDetailComponent = __decorate([
+                    router_1.CanActivate(function () { return TokenHelper_1.tokenNotExpired(); }),
+                    core_1.Component({
+                        selector: 'subThemes-details',
+                        directives: [router_1.ROUTER_DIRECTIVES, router_1.RouterLink],
+                        templateUrl: 'app/components/themes/subThemes/subThemeDetails.html',
+                    }), 
+                    __metadata('design:paramtypes', [])
+                ], SubThemeDetailComponent);
+                return SubThemeDetailComponent;
+            })();
+            exports_1("SubThemeDetailComponent", SubThemeDetailComponent);
+        }
+    }
+});
+//# sourceMappingURL=subThemeDetail.component.js.map
