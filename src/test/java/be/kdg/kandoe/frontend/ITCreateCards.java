1ddf6fc7c6edca02531e0145445a1ce811d9db85
Arne Lauryssens <arne.lauryssens@student.kdg.be>
2016-03-17 19:25:03 +0100

    selenium tests

--- src/test/java/be/kdg/kandoe/frontend/ITCreateCards.java
+++ src/test/java/be/kdg/kandoe/frontend/ITCreateCards.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -23,25 +24,45 @@ public class ITCreateCards {
         element = driver.findElement(By.id("password"));
         element.click();
         sendKeysPerCharacter(element, "test123");
-        element = driver.findElement(By.xpath("./*//*[@id='login-form']/div/button"));
+        element = driver.findElement(By.xpath(".//*[@id='login-form']/div/button"));
         element.click();
         (new WebDriverWait(driver, 10)).until((WebDriver d) -> d.findElement(By.tagName("loggedin-home")) != null);
         element = driver.findElement(By.linkText("THEMES"));
         element.click();
         (new WebDriverWait(driver, 10)).until((WebDriver d) -> d.findElement(By.tagName("theme")) != null);
+        /*(new WebDriverWait(driver, 10)).until(ExpectedConditions.visibilityOfElementLocated(
+                By.xpath("./*//*[@id='sort-list']/ul/li[2]/div/div/div[1]/div[2]/div[2]/button")));
         element = driver.findElement(By.xpath("./*//*[@id='sort-list']/ul/li[2]/div/div/div[1]/div[2]/div[2]/button"));
-        element.click();
-        (new WebDriverWait(driver, 10)).until((WebDriver d) -> d.findElement(By.id("addcard-btn")) != null);
-        element = driver.findElement(By.xpath("./*//*[@id='addcard-btn']"));
-        element.click();
-        (new WebDriverWait(driver, 10)).until(ExpectedConditions.visibilityOfElementLocated(
+        element.click();*/
+        /*(new WebDriverWait(driver, 10)).until((WebDriver d) -> d.findElement(By.xpath("./*//*[@id='expandes']/div[1]/button/a")) != null);
+        element = driver.findElement(By.xpath("./*//*[@id='expandes']/div[1]/button/a"));
+
+        Actions actions = new Actions(driver);
+        actions.moveToElement(element).click().perform();
+        //element.click();*/
+
+        /*(new WebDriverWait(driver, 10)).until(ExpectedConditions.visibilityOfElementLocated(
                 By.xpath("./*//*[@id='add-card']/div/div/div[2]/div/div/div/form/div[1]/input")));
         element = driver.findElement(By.xpath("./*//*[@id='add-card']/div/div/div[2]/div/div/div/form/div[1]/input"));
         sendKeysPerCharacter(element, "TestCard with selenium");
         element = driver.findElement(By.xpath("./*//*[@id='add-card']/div/div/div[3]/button"));
         element.click();
         (new WebDriverWait(driver, 10)).until((WebDriver d) -> d.findElement(
-                By.xpath("./*//*[@id='sort-list']/ul/li[2]/div/div/div[2]/div[2]/div/div[1]/div/ul/div[7]")) != null);
+                By.xpath(".*//*//**//*[@id='sort-list']/ul/li[2]/div/div/div[2]/div[2]/div/div[1]/div/ul/div[7]")) != null);*/
+        (new WebDriverWait(driver, 10)).until(ExpectedConditions.visibilityOfElementLocated(
+                By.xpath(".//*[@id='sort-list']/ul/li[2]/div/div/div[1]/div[1]/a/img")));
+        element = driver.findElement(By.xpath(".//*[@id='sort-list']/ul/li[2]/div/div/div[1]/div[1]/a/img"));
+        element.click();
+        (new WebDriverWait(driver, 10)).until(ExpectedConditions.visibilityOfElementLocated(
+                By.xpath("html/body/my-kandoe/theme-detail/div[1]/div[6]/div/h4/a")));
+        element = driver.findElement(By.xpath("html/body/my-kandoe/theme-detail/div[1]/div[6]/div/h4/a"));
+        element.click();
+        (new WebDriverWait(driver, 10)).until(ExpectedConditions.visibilityOfElementLocated(By.id("popupcard")));
+        element = driver.findElement(By.id("popupcard"));
+        sendKeysPerCharacter(element, "Testcard with selenium");
+        element = driver.findElement(By.xpath(".//*[@id='add-card']/div/div/div[3]/button"));
+        element.click();
+
     }
 
     private void sendKeysPerCharacter(WebElement element, String keys) {
